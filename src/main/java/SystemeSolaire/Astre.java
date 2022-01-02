880a9d617a7f157d23bbfbb8704edc6b3a44dde9
ShugoTenchi <ShugoTenchi@outlook.fr>
2022-01-02 15:42:53 +0100

    sdjfsqg

--- src/main/java/SystemeSolaire/Astre.java
+++ src/main/java/SystemeSolaire/Astre.java
@@ -12,12 +12,13 @@ public class Astre extends JPanel{
     private String nom;
     private double coordX;
     private double coordY;
-    private long temps;
-    private long vitesse;
+    private int vitesse;
+    private double angleDistance;
     private double rayon;
     private int periodeRevolution;
     private double taille;
     private Color couleur;
+    double angle ;
 
     public void setCoordX(double coordX) {
         this.coordX = coordX;
@@ -31,11 +32,7 @@ public class Astre extends JPanel{
         return nom;
     }
 
-    public long getTemps() {
-        return temps;
-    }
-
-    public long getVitesse() {
+    public int getVitesse() {
         return vitesse;
     }
 
@@ -65,14 +62,12 @@ public class Astre extends JPanel{
 
     public void ChangePosition(double angle){
         this.coordX = this.rayon * Math.sin(angle);
-        System.out.println("x =" + this.coordX);
         this.coordY = this.rayon * Math.cos(angle);
-        System.out.println("y =" + coordY);
     }
 
 
 
-    public Astre(String nom, double coordX, double coordY, double rayon, int periodeRevolution, double taille, Color couleur) {
+    public Astre(String nom, double coordX, double coordY, double rayon, int periodeRevolution, double angleVitesse, double taille, Color couleur, HorlogeUniverselle Horloge) {
         this.nom = nom;
         this.coordX = coordX;
         this.coordY = coordY;
@@ -80,12 +75,14 @@ public class Astre extends JPanel{
         this.periodeRevolution = periodeRevolution;
         this.taille = taille;
         this.couleur = couleur;
+        this.angle = Math.PI*1/2;
+        this.vitesse = this.periodeRevolution /  Horloge.getCombienDejour();
+        this.angleDistance = ((Math.PI*2)/this.periodeRevolution)*4.3;
     }
 
     public void placerAstre(){
         StdDraw.setPenRadius(this.taille);
         StdDraw.setPenColor(this.couleur);
-        System.out.println(this.coordY);
         StdDraw.filledCircle(this.coordX, this.coordY, 0.05);
     }
 
@@ -98,16 +95,24 @@ public class Astre extends JPanel{
     }
 
     public void deplacePosition(HorlogeUniverselle Horloge){
-        double angle = Math.PI*1/2;
-        int temps = Horloge.getSeconde() / 1000;
-        while (Horloge.getNombreDejour() < this.getPeriodeRevolution()){
-            StdDraw.clear(StdDraw.BLACK);
-            this.ChangePosition(angle);
-            angle = angle - 0.02;
-            MonSystemeSolaire.creeSysteme();
-            StdDraw.show();
-            StdDraw.pause(this.periodeRevolution/Horloge.getCombienDejour());
-        }
+        StdDraw.clear(StdDraw.BLACK);
+        this.ChangePosition(this.angle);
+        this.angle = this.angle - this.angleDistance;
+        MonSystemeSolaire.creeSysteme();
+        StdDraw.show();
+        HorlogeUniverselle.pause(360/Horloge.getCombienDejour());
     }
 
+    public static void deplaceSysteme(HorlogeUniverselle Horloge,Astre Mars, Astre Terre, Astre Venus, Astre Mercure, Astre Neptune, Astre Uranus, Astre Saturne, Astre Jupiter){
+        while (true){
+            Terre.deplacePosition(Horloge);
+            Mars.deplacePosition(Horloge);
+            Venus.deplacePosition(Horloge);
+            Mercure.deplacePosition(Horloge);
+            Neptune.deplacePosition(Horloge);
+            Uranus.deplacePosition(Horloge);
+            Saturne.deplacePosition(Horloge);
+            Jupiter.deplacePosition(Horloge);
+        }
+    }
 }
--- src/main/java/SystemeSolaire/MonSystemeSolaire.java
+++ src/main/java/SystemeSolaire/MonSystemeSolaire.java
@@ -5,15 +5,19 @@ import HorlogeUniverselle.HorlogeUniverselle;
 
 public class MonSystemeSolaire {
 
-    static Astre Soleil = new Astre("Soleil", 0,0,2, 0, 0.2, StdDraw.PRINCETON_ORANGE);
-    public static Astre Terre = new Astre("Terre", 0.85,0, 0.85,365, 0.025, StdDraw.BOOK_BLUE);
-    static Astre Mars = new Astre("Mars",1.0,0,1,686, 0.02, StdDraw.BOOK_RED);
-    static Astre Venus = new Astre("Venus", 0.7,0,0.7,224, 0.025, StdDraw.LIGHT_GRAY);
-    static Astre Mercure = new Astre("Mercure", 0.5,0, 0.5,87, 0.02, StdDraw.GRAY);
-    static Astre Jupiter = new Astre("Jupiter", 1.2,0, 1.2,4332,0.05, StdDraw.ORANGE);
-    static Astre Saturne = new Astre("Saturne", 1.45,0, 1.45,10759,0.05, StdDraw.YELLOW);
-    static Astre Uranus = new Astre("Uranus", 1.675,0, 1.675,30685,0.04,StdDraw.BOOK_LIGHT_BLUE);
-    static Astre Neptune = new Astre("Neptune", 1.875,0, 1.875,60266,0.04,StdDraw.BLUE);
+
+    static HorlogeUniverselle Horloge =
+            new HorlogeUniverselle(1, 10, 1000);
+
+    static Astre Soleil = new Astre("Soleil", 0,0,2, 0,0, 0.2, StdDraw.PRINCETON_ORANGE, Horloge);
+    public static Astre Terre = new Astre("Terre", 0.85,0, 0.85,365,0.02, 0.025, StdDraw.BOOK_BLUE, Horloge);
+    static Astre Mars = new Astre("Mars",1.0,0,1,686, 0.02,0.2, StdDraw.BOOK_RED, Horloge);
+    static Astre Venus = new Astre("Venus", 0.7,0,0.7,224,0.02, 0.025, StdDraw.LIGHT_GRAY, Horloge);
+    static Astre Mercure = new Astre("Mercure", 0.5,0, 0.5,87,0.02, 0.02, StdDraw.GRAY, Horloge);
+    static Astre Jupiter = new Astre("Jupiter", 1.2,0, 1.2,4332,0.02,0.05, StdDraw.ORANGE, Horloge);
+    static Astre Saturne = new Astre("Saturne", 1.45,0, 1.45,10759,0.02,0.05, StdDraw.YELLOW, Horloge);
+    static Astre Uranus = new Astre("Uranus", 1.675,0, 1.675,30685,0.02,0.04,StdDraw.BOOK_LIGHT_BLUE, Horloge);
+    static Astre Neptune = new Astre("Neptune", 1.875,0, 1.875,60266,0.02,0.04,StdDraw.BLUE, Horloge);
 
     static void creeSysteme(){
         Soleil.placerAstre();
@@ -40,24 +44,16 @@ public class MonSystemeSolaire {
     public static void main(String[] args) {
 
 
-        HorlogeUniverselle Horloge =
-                new HorlogeUniverselle(5, 1, 1000);
 
         StdDraw.clear(StdDraw.BLACK);
         creeSysteme();
         StdDraw.enableDoubleBuffering();
         StdDraw.show();
+
+
         Horloge.start();
 
-        Mars.deplacePosition(Horloge);
-        Terre.deplacePosition(Horloge);
-        Venus.deplacePosition(Horloge);
-        Mercure.deplacePosition(Horloge);
-        Neptune.deplacePosition(Horloge);
-        Uranus.deplacePosition(Horloge);
-        Saturne.deplacePosition(Horloge);
-        Jupiter.deplacePosition(Horloge);
-
+        Astre.deplaceSysteme(Horloge,Mars,Terre,Venus,Mercure,Neptune,Uranus,Saturne,Jupiter);
 
         creeSysteme();
         Horloge.fin();
